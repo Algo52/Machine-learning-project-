e9c538c27361bcc6cbc03bc8af210b0a354c9c4e
Algo52 <hemanthhemu5999@gmail.com>
2022-07-11 21:38:01 +0530

    update

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask
+import sys
+from housing.logger import logging
+from housing.exception import HousingException
 
 
 app=Flask(__name__)
@@ -6,6 +9,14 @@ app=Flask(__name__)
 
 @app.route("/",methods=["Get","Post"])
 def index():
+    try:
+        raise Exception("We are testing custom exception")
+    except Exception as e:
+        housing=HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("we are something to do")
+
+
     return "creating ci/cd pipeline project"
 
 if __name__=="__main__":
